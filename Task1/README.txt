07d16957fc006fb408029eb9e28fdef1d53bd885
Eric Schreiber <ericschr@student.ethz.ch>
2022-03-19 12:29:21 +0100

    Task1b & Task1a finished

---
+++ Task1/README.txt
@@ -0,0 +1,5 @@
+submission files:
+Task1a.py & submission1a.csv
+Task1bFinal.py & submission1bFinal.csv
+
+conversion ipynb to py: ipython nbconvert  --to script Task1b.ipynb
--- Task1/Task1bFinal.py
+++ Task1/Task1bFinal.py
@@ -92,48 +92,6 @@ print("Lowest RMSE:" + str(best_model.best_score_))
 best_alpha = best_model.best_estimator_
 print("With alpha: " + str(best_alpha))
 
-coefficiants = best_model.best_estimator_.coef_
-print("And coefficiants. ")
-print(coefficiants)
-
-
-# store
-# 
-
-# In[9]:
-
-
-dictionary = {'Coefficiants': coefficiants}
-avg_acc_df = pd.DataFrame(dictionary )
-print(avg_acc_df)
-avg_acc_df.to_csv(r'C:\Users\erics\Documents\Programme\IntroML\Task1\submission1b.csv', index=False, header=False)
-
-
-# In[14]:
-
-
-model2 = Ridge(alpha=5.170920242896756)
-best_fit = model2.fit(X_trans, Y)
-coef = best_fit.coef_
-print(coef)
-
-
-# Overfitting Data This does not work
-
-# In[17]:
-
-
-model3 = LinearRegression()
-best_fit = model3.fit(X_trans, Y)
-coef = best_fit.coef_
-print(coef)
-
-
-# Versuche nochmals 10 fold cross validation
-
-# In[10]:
-
-
 k = 10
 kf = KFold(n_splits=k, random_state=None)
 model4 = Ridge(alpha=5.170920242896756, fit_intercept=False)
@@ -178,7 +136,7 @@ print(model4.coef_)
 dictionary = {'Coefficiants': model4.coef_}
 avg_acc_df = pd.DataFrame(dictionary )
 print(avg_acc_df)
-avg_acc_df.to_csv(r'C:\Users\erics\Documents\Programme\IntroML\Task1\submission1bV4.csv', index=False, header=False)
+avg_acc_df.to_csv(r'C:\Users\erics\Documents\Programme\IntroML\Task1\submission1bFinal.csv', index=False, header=False)
 
 
 # In[ ]:
